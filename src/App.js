ca236a041bcd2a913b8a8529fb720e0b194a14c6
porideki <porideki@gmail.com>
2020-10-25 03:24:47 +0900

    function App change to class App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <header>
-        <h1 class="title">ToDo</h1>
-        <div class="add-button">+</div>
-      </header>
-      <div id="main"></div>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1 class="title">ToDo</h1>
+          <div class="add-button">+</div>
+        </header>
+        <div id="main"></div>
+      </div>
+    );
+  }
 }
 
 export default App;
