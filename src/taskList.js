9d0050ee2a73a4a4fc05dc67dff9195563d76325
porideki <porideki@gmail.com>
2020-10-27 02:37:12 +0900

    add edit task function

--- src/taskList.js
+++ src/taskList.js
@@ -28,6 +28,8 @@ function TaskElement(props) {
   var {task, app} = props;
   const tasks = app.state.tasks;
   //タスク操作
+  const [taskTitle, setTaskTitle] = useState("");
+  const [taskDescription, setTaskDescription] = useState("");
   const onClickDelete = (event) => {
     //ボタンの属性からタスクIDを取得
     var target = event.target;
@@ -39,14 +41,60 @@ function TaskElement(props) {
       }
     });
   };
+  const handleTaskSubmit = (event) => {
+    //更新対象のタスクのインデクスを取得
+    var targetIndex = 0;
+    tasks.forEach((element, index) => {
+      //更新対象のタスクに一致
+      if(element.id == task.id){
+        targetIndex = index;
+      }
+    });
+    //上書き
+    tasks[targetIndex] = {
+      id:task.id,
+      title:event.target.getAttribute("title"),
+      description:event.target.getAttribute("description")
+    }
+    //リストに反映
+    app.setState({tasks:tasks});
+    closeModal();
+  };
+  //モーダル関係
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const openModal = () => {
+    setTaskTitle(task.title);
+    setTaskDescription(task.description);
+    setIsOpenModal(true)
+  };
+  const closeModal = () => {setIsOpenModal(false)};
 
   return (
     <div class="task-element">
-      <div class="task-title-desc">
+      {/* タスク表示 */}
+      <div class="task-title-desc" onClick={openModal}>
         <h2 class="task-title">{task.title}</h2>
         <p class="task-description">{task.description}</p>
       </div>
       <div class="task-del-button" taskid={task.id} onClick={(event)=>onClickDelete(event)}>x</div>
+      {/* モーダルウィンドウ */}
+      <Modal
+      isOpen={isOpenModal}
+      onRequestClose={closeModal}>
+          <h3>Edit Task</h3>
+          <div>
+            <h4>Task Name</h4>
+            <input id="task-title-input" type="text" placeholder="input task title" value={taskTitle} onChange={(event) => {setTaskTitle(event.target.value)}}/>
+          </div>
+          <div>
+            <h4>Description</h4>
+            <input id="task-description-input" type="text" placeholder="input task description" value={taskDescription} onChange={(event) => {setTaskDescription(event.target.value)}}/>
+          </div>
+          <div>
+            <button onClick={closeModal}>cansel</button>
+            <button onClick={handleTaskSubmit} title={taskTitle} description={taskDescription}>confirm</button>
+          </div>
+      </Modal>
     </div>
   );
 }
