75338dc613bea0a5453ab5bda75314cd3ae53821
porideki <porideki@gmail.com>
2020-10-27 01:52:59 +0900

    task rendering is moved to component function

--- src/taskList.js
+++ src/taskList.js
@@ -1,46 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Modal from 'react-modal'
 
 class TaskList extends React.Component {
   render() {
-    const {tasks} = this.props;
+    const {app} = this.props;
+    const tasks = app.state.tasks
     return (
       tasks.map((task) => {
         return (
           <div class="task-element-edge">
-            {this.rendtask(task)}
+            {this.rendtask(task, app)}
           </div>
         );
       })
     );
   }
 
-  rendtask(task) {
+  rendtask(task, app) {
     return (
-      <div class="task-element">
-        <div class="task-title-desc">
-          <h2 class="task-title">{task.title}</h2>
-          <p class="task-description">{task.description}</p>
-        </div>
-        <div class="task-del-button" taskid={task.id} onClick={(event)=>this.onClickDelete(event)}>x</div>
-      </div>
+      <TaskElement task={task} app={app}/>
     );
   }
 
-  onClickDelete(event) {
+}
+
+function TaskElement(props) {
+  var {task, app} = props;
+  const tasks = app.state.tasks;
+  //タスク操作
+  const onClickDelete = (event) => {
     //ボタンの属性からタスクIDを取得
     var target = event.target;
     var id = target.getAttribute("taskid");
-    //IDに一致するタスクを削除
-    const {tasks, app} = this.props;
-    var newTasks = [];
     tasks.forEach((element) => {
       //タスクIDが削除対象IDと一致
       if(element.id == id){
         app.delTask(element);
       }
     });
-  }
+  };
 
+  return (
+    <div class="task-element">
+      <div class="task-title-desc">
+        <h2 class="task-title">{task.title}</h2>
+        <p class="task-description">{task.description}</p>
+      </div>
+      <div class="task-del-button" taskid={task.id} onClick={(event)=>onClickDelete(event)}>x</div>
+    </div>
+  );
 }
 
 export default TaskList;
